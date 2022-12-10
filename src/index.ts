8e3b32496f8326a1e11406103dc68ac579076535
Sahil Gupta <sahil865gupta@gmail.com>
2022-12-11 00:11:39 +0530

    chore: added IConfigFetcher export in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import IConfigFetcher from "./interfaces/configFetcherInterface";
 import { setupMockServer, startMockServer } from "./main";
 
 export {
     startMockServer,
     setupMockServer,
+    IConfigFetcher,
 };
