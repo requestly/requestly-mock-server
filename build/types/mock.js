e57e27e5e3cc16d56b93739d49b7174acf3317f6
Sahil Gupta <sahil865gupta@gmail.com>
2022-12-19 19:34:14 +0530

    Revert "chore: added MockType enum"
    This reverts commit 87a08ade650a83487d53ec4e18dadd018224d701.

--- build/types/mock.js
+++ build/types/mock.js
@@ -1,8 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MockType = void 0;
-var MockType;
-(function (MockType) {
-    MockType["API"] = "API";
-    MockType["FILE"] = "FILE";
-})(MockType = exports.MockType || (exports.MockType = {}));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import IConfigFetcher from "./interfaces/configFetcherInterface";
 import { setupMockServer, startMockServer } from "./main";
-import {
-    Mock as MockSchema,
-    MockMetadata as MockMetadataSchema,
-    Response as MockResponseSchema,
-    MockType 
-} from "./types/mock";
+import { Mock as MockSchema, MockMetadata as MockMetadataSchema, Response as MockResponseSchema } from "./types/mock";
 
 export {
     startMockServer,
@@ -14,5 +9,4 @@ export {
     MockSchema,
     MockMetadataSchema,
     MockResponseSchema,
-    MockType,
 };
