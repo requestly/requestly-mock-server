aef508baf721fd4156f5a700f31021f3bace7000
Sahil Gupta <sahil865gupta@gmail.com>
2023-06-26 16:27:20 +0530

    chore: minor fix

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,4 +1,6 @@
 import MockServer from "../core/server";
 import firebaseConfigFetcher from "./firebaseConfigFetcher";
 
-new MockServer(3000, firebaseConfigFetcher, "/mocksv2").start();
+const server = new MockServer(3000, firebaseConfigFetcher, "/mocksv2");
+console.log(server.app);
+server.start();
