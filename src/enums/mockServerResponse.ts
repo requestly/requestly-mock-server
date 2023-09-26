9f127beeb3a60146ceb588e5a7f3aa2b8a1919cb
Gagan Ganapathy Ajjikuttira <gajjikuttira@sumologic.com>
2023-09-25 21:27:27 -0700

    [RQ-OSS-1050: Add more status codes to HttpStatusCode

--- src/enums/mockServerResponse.ts
+++ src/enums/mockServerResponse.ts
@@ -1,4 +1,13 @@
 export enum HttpStatusCode {
+    OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+
+    BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
-    NOT_FOUND = 404
+    NOT_FOUND = 404,
+
+    INTERNAL_SERVER_ERROR = 500,
+    NOT_IMPLEMENTED = 501,
+    BAD_GATEWAY = 502,
 }
