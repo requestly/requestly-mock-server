8237aadea11269526687b9d54b5ff90e20691cc4
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-08-13 09:58:25 +0530

    feat: expose log type from package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import IConfigFetcher from "./interfaces/configFetcherInterface";
 import IlogSink from "./interfaces/logSinkInterface";
 import MockServer from "./core/server";
 import { Mock as MockSchema, MockMetadata as MockMetadataSchema, Response as MockResponseSchema } from "./types/mock";
-
+import {Log as MockLog} from "./types";
 export {
     MockServer,
     IConfigFetcher,
@@ -10,4 +10,5 @@ export {
     MockSchema,
     MockMetadataSchema,
     MockResponseSchema,
+    MockLog,
 };
