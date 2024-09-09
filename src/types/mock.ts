67a840d21fd44a2e4ebf3e6a8b9352761479b813
rohanmathur91 <mathurrohan04@gmail.com>
2024-09-09 13:41:06 +0530

    chore: fix types

--- src/types/mock.ts
+++ src/types/mock.ts
@@ -2,29 +2,29 @@ import { RequestMethod } from ".";
 
 // TODO: Convert into classes later on
 export interface Mock extends MockMetadata {
-    responses: Response[] // Right now we are keeping only 1 response
+  responses: Response[]; // Right now we are keeping only 1 response
 }
 
 // Useful when fetching list of mocks. Saves network bandwith
 export interface MockMetadata {
-    id : string
-    name?: string
-    desc ?: string
-    method: RequestMethod
-    endpoint: string
-    ownerId?: string
-    createdTs?: string
-    updatedTs?: string
-    password?: string
+  id: string;
+  name: string;
+  desc?: string;
+  method: RequestMethod;
+  endpoint: string;
+  ownerId: string;
+  createdTs?: number;
+  updatedTs?: number;
+  password?: string;
 }
 
 export interface Response {
-    id: string
-    desc ?: string
-    latency ?: number
-    statusCode: number // TODO: Change this to list of acceptable status codes
-    headers: {[key: string]: string}
-    body: string
-    filePath?: string,
-    // rules: []
+  id: string;
+  desc?: string;
+  latency?: number;
+  statusCode: number; // TODO: Change this to list of acceptable status codes
+  headers: { [key: string]: string };
+  body: string;
+  filePath?: string;
+  // rules: []
 }
