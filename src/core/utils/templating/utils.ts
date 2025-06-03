cce5e9371b7a4e9be5f7e48c4b681f8c74e57823
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-03 20:40:25 +0530

    fix: template helper name matching

--- src/core/utils/templating/utils.ts
+++ src/core/utils/templating/utils.ts
@@ -1,20 +1,43 @@
 /**
  * if match is a string like {{unknownHelper arg1 arg2}}
  * it will add `\\` to its prefix, signaling handlebars should ignore it
- * 
+ *
  * https://handlebarsjs.com/guide/expressions.html#escaping-handlebars-expressions
  */
 function escapeMatchFromHandlebars(match: string) {
-    return match.replace(/({{)/g, '\\$1');
+  return match.replace(/({{)/g, "\\$1");
 }
 
-export function wrapUnexpectedTemplateCaptures(template: string, allHelpers: Record<string, unknown>) {
-    const helperNames = Object.keys(allHelpers)
-    return template.replace(/{{\s*([\s\S]*?)\s*}}/g, (completeMatch, firstMatchedGroup) => {
-        const isMatchEmpty = firstMatchedGroup.trim() === ''; // {{}}
-        const matchStartsWithKnownHelper = helperNames.some(helperName => firstMatchedGroup.includes(helperName));
+/**
+ * Creates regex to match pattern: "helperName 'parameter'"
+ */
+function getRegexByHelperName(helperName: string) {
+  return new RegExp(`${helperName}\\b\\s+['"][^'"]*['"]`, "g");
+}
 
-        if(isMatchEmpty || !matchStartsWithKnownHelper) return escapeMatchFromHandlebars(completeMatch);
-        else return completeMatch;
-    });
+function isMatchesHelperName(helperName: string, templateString: string) {
+  const regex = getRegexByHelperName(helperName);
+  return regex.test(templateString);
+}
+
+export function wrapUnexpectedTemplateCaptures(
+  template: string,
+  allHelpers: Record<string, unknown>
+) {
+  const helperNames = Object.keys(allHelpers);
+  return template.replace(
+    /{{\s*([\s\S]*?)\s*}}/g,
+    (completeMatch, firstMatchedGroup) => {
+      const isMatchEmpty = firstMatchedGroup.trim() === ""; // {{}}
+      const matchContainsKnownHelper = helperNames.some((helperName) => {
+        return isMatchesHelperName(helperName, firstMatchedGroup);
+      });
+
+      if (isMatchEmpty || !matchContainsKnownHelper) {
+        return escapeMatchFromHandlebars(completeMatch);
+      } else {
+        return completeMatch;
+      }
+    }
+  );
 }
